cf1f8e36c5dc31cdc53a4ef63277fd9d07df714e
akshaykapadi <akshaykapadi8@gmail.com>
2023-06-03 17:16:43 +0530

    jquery import

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete") {
       chrome.scripting.executeScript({
         target: { tabId: tabId },
-        files: ["content.js"]
+        files: ["jquery.min.js", "content.js"]
       });
     }
   });
